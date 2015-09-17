99beeeed20e764b1dc158e2365b4a859b50b2679
Bramus <bramus@bram.us>
2015-09-17 15:11:54 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# ion-drawer-vertical
+
+A vertical drawer for Ionic
+
+## Here be dragons
+
+Do note that this is an early release of `ion-drawer-vertical`. Many things are missing _(Installation via `bower`, make the drawer follow the drag position, etc. to name a few)_. Things might change in the future. Or they might remain the same. The plugin works for me. It might not work for you.
+
+## Installation
+
+Add the `ionic.contrib.drawer.vertical.js` and `ionic.contrib.drawer.vertical.css` files to your index file
+
+```
+<link href="ion-drawer-vertical/ionic.contrib.drawer.vertical.css" rel="stylesheet">
+<script src="ion-drawer-vertical/ionic.contrib.drawer.vertical.js"></script>
+```
+
+Add `ionic.contrib.drawer.vertical` to your Angular app:
+
+```
+angular.module('app', [
+	'ionic',
+	'ionic.contrib.drawer'
+])
+```
+
+## Usage
+
+Add a `<ion-drawer-vertical-wrapper>` element to your document. Inside it, put a `<ion-drawer-vertical-content>` and a `<ion-drawer-vertical-handle>` element.
+
+### Configuration
+
+Adjust the `direction` _(possible values: `down` [default] and `up`)_ and `state` _(possible values: `opened` [default] and `closed`)_ attributes of the `<ion-drawer-vertical-handle>` element if need.
+
+Set the proper `has-*` classes _(such as `has-header` or `has-footer`)_ on the `<ion-drawer-vertical-wrapper>` element if any headers and/or footers are present.
+
+### Example
+
+```
+	<ion-drawer-vertical-wrapper class="has-header has-footer">
+		<ion-drawer-vertical-content>
+			<img src="http://lorempixel.com/g/400/200/animals/4/horizontal-giraffe/" alt="Horizontal Giraffe" title="Horizontal Giraffe" />
+		</ion-drawer-vertical-content>
+		<ion-drawer-vertical-handle direction="down" state="closed" />
+	</ion-drawer-vertical-wrapper>
+```
+
+### Events and Functions
+
+`ion-drawer-vertical` automatically binds `dragup` and `dragdown` events to the `<ion-drawer-vertical-handle>` element. Dragging said element will alter the opened/closed state of the drawer.
+
+The functions `openDrawer()`, `closeDrawer()` and `toggleDrawer()` are exposed into the `$scope` if one wants to manually call these.
+
+## Acknowledgements
+
+[`ionic-contrib-drawer`](https://github.com/driftyco/ionic-ion-drawer) which formed a good starting point for `ion-drawer-vertical`.
+
+## License
+
+`ion-drawer-vertical` is released under the MIT public license. See the enclosed `LICENSE` for details.
---
+++ ion-drawer-vertical/ionic.contrib.drawer.vertical.js
@@ -0,0 +1,157 @@
+(function() {
+
+'use strict';
+
+	angular.module('ionic.contrib.drawer.vertical', ['ionic'])
+
+	.controller('$ionDrawerVerticalHandle', function($element, $attrs, $ionicGesture, $timeout) {
+
+		// We need closure
+		var self = this;
+
+		// Possible states the drawer can have
+		var STATE_CLOSE = 'closed';
+		var STATE_OPEN = 'opened';
+
+		// Possible directions the drawer may slide out to
+		var DIRECTION_DOWN = 'down';
+		var DIRECTION_UP = 'up';
+
+		// Get state & direction
+		// default: STATE_OPEN and DIRECTION_DOWN
+		var state = ($attrs.state === STATE_CLOSE ? STATE_CLOSE : STATE_OPEN);
+		var direction = ($attrs.direction === DIRECTION_UP ? DIRECTION_UP : DIRECTION_DOWN);
+
+		// Parameter which tells if we are animating or not
+		var isBusyAnimating = false;
+
+		// Persist the state and direction on the wrapper
+		// (needed for animations)
+		var $wrapper = $element.parent('ion-drawer-vertical-wrapper');
+	    $wrapper.addClass(state);
+	    $wrapper.addClass(direction);
+
+	    // Open the drawer
+		var open = function() {
+			if (!this.isOpen() || isBusyAnimating) {
+				isBusyAnimating = true;
+				$wrapper.removeClass(STATE_CLOSE);
+				$wrapper.addClass(STATE_OPEN + ' animate');
+				$timeout(function() {
+					$wrapper.removeClass('animate');
+					state = STATE_OPEN;
+					isBusyAnimating = false;
+				}, 400);
+			}
+		}
+		this.open = open;
+
+		// Close the drawer
+		var close = function() {
+			if (this.isOpen() || isBusyAnimating) {
+				isBusyAnimating = true;
+				$wrapper.removeClass(STATE_OPEN);
+				$wrapper.addClass(STATE_CLOSE + ' animate');
+				$timeout(function() {
+					$wrapper.removeClass('animate');
+					state = STATE_CLOSE;
+					isBusyAnimating = false;
+				}, 400);
+			}
+		}
+		this.close = close;
+
+		// Toggle the drawer
+		var toggle = function() {
+			if (this.isOpen()) {
+				this.close();
+			} else {
+				this.open();
+			}
+		}
+		this.toggle = toggle;
+
+		// Check if the drawer is open or not
+		var isOpen = function() {
+			return state === STATE_OPEN;
+		}
+		this.isOpen = isOpen;
+
+		// Handle drag up event: open or close (based on direction)
+		$ionicGesture.on('dragup', function(e) {
+
+			// Don't do jack if we are already animating
+			if (isBusyAnimating) return;
+
+			// Drawer needs to slide up in order to be open, and state is not open: open it!
+			if ((direction == DIRECTION_UP) && !self.isOpen()) {
+				self.open();
+				return;
+			}
+
+			// Drawer needs to slide up in order to be closed, and state is open (not closed): close it!
+			if ((direction == DIRECTION_DOWN) && self.isOpen()) {
+				self.close();
+				return;
+			}
+
+		}, $element);
+
+		// Handle drag down event: open or close (based on direction)
+		$ionicGesture.on('dragdown', function(e) {
+
+			// Don't do jack if we are already animating
+			if (isBusyAnimating) return;
+
+			// Drawer needs to slide down in order to be open, and state is not open: open it!
+			if ((direction == DIRECTION_DOWN) && !self.isOpen()) {
+				self.open();
+				return;
+			}
+
+			// Drawer needs to slide up in order to be closed, and state is open (not closed): close it!
+			if ((direction == DIRECTION_UP) && self.isOpen()) {
+				self.close();
+				return;
+			}
+
+		}, $element);
+
+	});
+
+})();
+
+(function() {
+
+	angular.module('ionic.contrib.drawer.vertical')
+
+	.directive('ionDrawerVerticalWrapper', function() {
+		return {
+			restrict: 'E'
+		}
+	})
+
+	.directive('ionDrawerVerticalHandle', function() {
+		return {
+			restrict: 'E',
+			controller: '$ionDrawerVerticalHandle',
+			link: function($scope, $element, $attr, ctrl) {
+
+				$scope.openDrawer = function() {
+					ctrl.open();
+				};
+
+				$scope.closeDrawer = function() {
+					ctrl.close();
+				};
+
+				$scope.toggleDrawer = function() {
+					ctrl.toggle();
+				};
+
+			}
+		}
+	});
+
+
+})();
